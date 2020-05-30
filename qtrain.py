67a4bdba7700a4c364b32a677baff34fc9953132
huangsx95 <30683101+huangsx95@users.noreply.github.com>
2020-05-30 18:10:48 +0800

    Add files via upload

--- qtrain.py
+++ qtrain.py
@@ -29,24 +29,9 @@ torch.manual_seed(191009)
 from torch.quantization import QuantStub, DeQuantStub
 
 
-def _make_divisible(v, divisor, min_value=None):
-    """
-    This function is taken from the original tf repo.
-    It ensures that all layers have a channel number that is divisible by 8
-    It can be seen here:
-    https://github.com/tensorflow/models/blob/master/research/slim/nets/mobilenet/mobilenet.py
-    :param v:
-    :param divisor:
-    :param min_value:
-    :return:
-    """
-    if min_value is None:
-        min_value = divisor
-    new_v = max(min_value, int(v + divisor / 2) // divisor * divisor)
-    # Make sure that round down does not go down by more than 10%.
-    if new_v < 0.9 * v:
-        new_v += divisor
-    return new_v
+cfg = {
+    'G1':[16, 'M2', 32, 'M2', 64, 'M2', 128, 'M2', 256, 'M2', 512, 1024],
+}
 
 
 class ConvBNReLU(nn.Sequential):
@@ -60,94 +45,13 @@ class ConvBNReLU(nn.Sequential):
         )
 
 
-class InvertedResidual(nn.Module):
-    def __init__(self, inp, oup, stride, expand_ratio):
-        super(InvertedResidual, self).__init__()
-        self.stride = stride
-        assert stride in [1, 2]
-
-        hidden_dim = int(round(inp * expand_ratio))
-        self.use_res_connect = self.stride == 1 and inp == oup
-
-        layers = []
-        if expand_ratio != 1:
-            # pw
-            layers.append(ConvBNReLU(inp, hidden_dim, kernel_size=1))
-        layers.extend([
-            # dw
-            ConvBNReLU(hidden_dim, hidden_dim, stride=stride, groups=hidden_dim),
-            # pw-linear
-            nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
-            nn.BatchNorm2d(oup, momentum=0.1),
-        ])
-        self.conv = nn.Sequential(*layers)
-        # Replace torch.add with floatfunctional
-        self.skip_add = nn.quantized.FloatFunctional()
-
-    def forward(self, x):
-        if self.use_res_connect:
-            return self.skip_add.add(x, self.conv(x))
-        else:
-            return self.conv(x)
-
-
-class MobileNetV2(nn.Module):
-    def __init__(self, num_classes=1000, width_mult=1.0, inverted_residual_setting=None, round_nearest=8):
-        """
-        MobileNet V2 main class
-
-        Args:
-            num_classes (int): Number of classes
-            width_mult (float): Width multiplier - adjusts number of channels in each layer by this amount
-            inverted_residual_setting: Network structure
-            round_nearest (int): Round the number of channels in each layer to be a multiple of this number
-            Set to 1 to turn off rounding
-        """
-        super(MobileNetV2, self).__init__()
-        block = InvertedResidual
-        input_channel = 32
-        last_channel = 1280
-
-        if inverted_residual_setting is None:
-            inverted_residual_setting = [
-                # t, c, n, s
-                [1, 16, 1, 1],
-                [6, 24, 2, 2],
-                [6, 32, 3, 2],
-                [6, 64, 4, 2],
-                [6, 96, 3, 1],
-                [6, 160, 3, 2],
-                [6, 320, 1, 1],
-            ]
-
-        # only check the first element, assuming user knows t,c,n,s are required
-        if len(inverted_residual_setting) == 0 or len(inverted_residual_setting[0]) != 4:
-            raise ValueError("inverted_residual_setting should be non-empty "
-                             "or a 4-element list, got {}".format(inverted_residual_setting))
-
-        # building first layer
-        input_channel = _make_divisible(input_channel * width_mult, round_nearest)
-        self.last_channel = _make_divisible(last_channel * max(1.0, width_mult), round_nearest)
-        features = [ConvBNReLU(3, input_channel, stride=2)]
-        # building inverted residual blocks
-        for t, c, n, s in inverted_residual_setting:
-            output_channel = _make_divisible(c * width_mult, round_nearest)
-            for i in range(n):
-                stride = s if i == 0 else 1
-                features.append(block(input_channel, output_channel, stride, expand_ratio=t))
-                input_channel = output_channel
-        # building last several layers
-        features.append(ConvBNReLU(input_channel, self.last_channel, kernel_size=1))
-        # make it nn.Sequential
-        self.features = nn.Sequential(*features)
+class GNET(nn.Module):
+    def __init__(self, vgg_name):
+        super(GNET, self).__init__()
+        self.features = self._make_layers(cfg[vgg_name])
         self.quant = QuantStub()
         self.dequant = DeQuantStub()
-        # building classifier
-        self.classifier = nn.Sequential(
-            nn.Dropout(0.2),
-            nn.Linear(self.last_channel, num_classes),
-        )
-
+        
         # weight initialization
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -162,25 +66,32 @@ class MobileNetV2(nn.Module):
                 nn.init.zeros_(m.bias)
 
     def forward(self, x):
-
         x = self.quant(x)
-
         x = self.features(x)
-        x = x.mean([2, 3])
-        x = self.classifier(x)
+        x = out.view(x.size(0), -1)
+        x = F.softmax(x, dim=0)
         x = self.dequant(x)
         return x
 
-    # Fuse Conv+BN and Conv+BN+Relu modules prior to quantization
-    # This operation does not change the numerics
+    def _make_layers(self, cfg):
+        layers = []
+        in_channels = 3
+        for x in cfg:
+            if x == 'M2':
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            else:
+                layers += [ConvBNReLU(in_channels, x)]
+                in_channels = x
+        layers += [ConvBNReLU(in_planes=in_channels, out_planes=256, kernel_size=1)]
+        layers += [ConvBNReLU(in_planes=256, out_planes=512, kernel_size=3)]
+        layers += [nn.Conv2d(512, 10, 1, 1, groups=1, bias=False)]
+        layers += [nn.AvgPool2d(kernel_size=13, stride=1)]
+        return nn.Sequential(*layers)
+
     def fuse_model(self):
         for m in self.modules():
             if type(m) == ConvBNReLU:
                 torch.quantization.fuse_modules(m, ['0', '1', '2'], inplace=True)
-            if type(m) == InvertedResidual:
-                for idx in range(len(m.conv)):
-                    if type(m.conv[idx]) == nn.Conv2d:
-                        torch.quantization.fuse_modules(m.conv, [str(idx), str(idx + 1)], inplace=True)
 
 
 class AverageMeter(object):
@@ -231,7 +142,7 @@ def evaluate(model, criterion, data_loader, neval_batches):
     cnt = 0
     with torch.no_grad():
         for image, target in data_loader:
-            image, target = image.to('cuda:0'), target.to('cuda:0')
+            image, target = image.to('cuda:0,1,2,3'), target.to('cuda:0,1,2,3')
             output = model(image)
             loss = criterion(output, target)
             cnt += 1
@@ -243,14 +154,6 @@ def evaluate(model, criterion, data_loader, neval_batches):
                 return top1, top5
 
     return top1, top5
-
-
-def load_model(model_file):
-    model = MobileNetV2()
-    state_dict = torch.load(model_file)
-    model.load_state_dict(state_dict)
-    model.to('cpu')
-    return model
 
 
 def print_size_of_model(model):
@@ -268,7 +171,8 @@ def prepare_data_loaders(data_path):
     dataset = torchvision.datasets.ImageFolder(
         traindir,
         transforms.Compose([
-            transforms.RandomResizedCrop(224),
+            transforms.Resize(420),
+            transforms.RandomResizedCrop(416),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             normalize,
@@ -277,8 +181,8 @@ def prepare_data_loaders(data_path):
     dataset_test = torchvision.datasets.ImageFolder(
         valdir,
         transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
+            transforms.Resize(420),
+            transforms.CenterCrop(416),
             transforms.ToTensor(),
             normalize,
         ]))
@@ -299,9 +203,7 @@ def prepare_data_loaders(data_path):
 
 data_path = '/home/f321/data/ILSVRC2012'
 saved_model_dir = './data/'
-float_model_file = 'mobilenet_pretrained_float.pth'
-scripted_float_model_file = 'mobilenet_quantization_scripted.pth'
-scripted_quantized_model_file = 'mobilenet_quantization_scripted_quantized.pth'
+scripted_quantized_model_file = 'gnet_quantization_scripted_quantized.pth'
 
 train_batch_size = 80
 eval_batch_size = 100
@@ -347,7 +249,7 @@ def train_one_epoch(model, criterion, optimizer, data_loader, device, ntrain_bat
     return
 
 
-qat_model = load_model(saved_model_dir + float_model_file).to('cuda:0')
+qat_model = GNET('G1').to('cuda:0,1,2,3')
 qat_model.fuse_model()
 optimizer = torch.optim.SGD(qat_model.parameters(), lr=0.0001)
 qat_model.qconfig = torch.quantization.get_default_qat_qconfig('fbgemm')
@@ -360,7 +262,7 @@ num_train_batches = 20
 max_acc = 0.0
 # Train and check accuracy after each epoch
 for nepoch in range(20):
-    train_one_epoch(qat_model, criterion, optimizer, data_loader, torch.device('cuda:0'), num_train_batches)
+    train_one_epoch(qat_model, criterion, optimizer, data_loader, torch.device('cuda:0,1,2,3'), num_train_batches)
     if nepoch > 3:
         # Freeze quantizer parameters
         qat_model.apply(torch.quantization.disable_observer)
@@ -375,5 +277,5 @@ for nepoch in range(20):
         max_acc = top1.avg
         quantized_model = torch.quantization.convert(qat_model.to('cpu'), inplace=False)
         torch.jit.save(torch.jit.script(quantized_model), saved_model_dir + scripted_quantized_model_file)
-        qat_model.to('cuda:0')
+        qat_model.to('cuda:0,1,2,3')
     print('Epoch %d :Evaluation accuracy on %d images, %2.2f'%(nepoch, num_eval_batches * eval_batch_size, top1.avg))
